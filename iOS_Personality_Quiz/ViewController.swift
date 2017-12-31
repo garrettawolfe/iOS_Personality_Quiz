0170d538c831b89688430b1490881c1ae053c318
Garrett Wolfe <gaw13@duke.edu>
2017-12-31 15:09:13 +1300

    Initial Commit

---
+++ iOS_Personality_Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Personality_Quiz
+//
+//  Created by Garrett Wolfe on 12/31/17.
+//  Copyright © 2017 Garrett Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
